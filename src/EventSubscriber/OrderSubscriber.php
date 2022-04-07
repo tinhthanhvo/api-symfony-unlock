685cb9bf99f3d5ba4c487c9826632e8e82a09c6a
Thanh Vo <tinhthanh.vo@nfq.asia>
2022-04-07 22:19:16 +0700

    update send mail to admin when user book

--- src/EventSubscriber/OrderSubscriber.php
+++ src/EventSubscriber/OrderSubscriber.php
@@ -19,7 +19,7 @@ class OrderSubscriber implements EventSubscriberInterface
         $this->mailerService = $mailerService;
     }
 
-    public function onSendOrder(PurchaseOrderEvent $event)
+    public function onSendOrderDetailToUser(PurchaseOrderEvent $event)
     {
         $order = $event->getPurchaseOrder();
 
@@ -35,11 +35,30 @@ class OrderSubscriber implements EventSubscriberInterface
             $params
         );
     }
-    public static function getSubscribedEvents()
+
+    public function onSendOrderDetailToAdmin(PurchaseOrderEvent $event)
+    {
+        $order = $event->getPurchaseOrder();
+
+        $params = [
+            "order" => $order
+        ];
+
+        $this->mailerService->send(
+            'Confirm order information',
+            'tinhthanh2210@gmail.com',
+            'tinhthanh.vo@nfq.asia',
+            PurchaseOrderEvent::TEMPLATE_CONTACT,
+            $params
+        );
+    }
+
+    public static function getSubscribedEvents(): array
     {
         return[
             PurchaseOrderEvent::class => [
-                ['onSendOrder', 1]
+                ['onSendOrderDetailToUser', 1],
+                ['onSendOrderDetailToAdmin', 1]
             ]
         ];
     }
