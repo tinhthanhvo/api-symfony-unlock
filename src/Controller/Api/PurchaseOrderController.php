7103f3837570725d97c349f4f06e8469c70df8db
Thanh Vo <tinhthanh.vo@nfq.asia>
2022-04-08 15:19:04 +0700

    apply test for order by user

--- src/Controller/Api/PurchaseOrderController.php
+++ src/Controller/Api/PurchaseOrderController.php
@@ -80,7 +80,7 @@ class PurchaseOrderController extends AbstractFOSRestController
     {
         $order = new PurchaseOrder($this->userLoginInfo);
         $form = $this->createForm(PurchaseOrderType::class, $order);
-        $requestData = $request->request->all();
+        $requestData = json_decode($request->getContent(), true);
         $form->submit($requestData);
 
         $totalPrice = 0;
